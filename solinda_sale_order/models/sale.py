76abcb41617770a41d560666aba9b9c00ee5351e
inicoding <irfannurrahmat@outlook.com>
2022-11-08 18:31:02 +0700

    typo

--- solinda_sale_order/models/sale.py
+++ solinda_sale_order/models/sale.py
@@ -18,7 +18,7 @@ class SaleOrder(models.Model):
     director_info = fields.Char(string='Director')
     
     def action_print_quotation_boo(self):
-        return self.env.ref('gls_reporting.report_quotation_boo_actiono').report_action(self)
+        return self.env.ref('gls_reporting.report_quotation_boo_action').report_action(self)
 
     def action_print_quotation_oms(self):
         return self.env.ref('gls_reporting.report_quotation_oms_action').report_action(self)
