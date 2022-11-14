4ddacfdf5e52e6d593c37d550404fba91953cd82
Luthfi Achmaddin Nizar <Luthfinizar97@gmail.com>
2022-11-14 17:58:12 +0700

    not required if SO is from cost sheet

--- solinda_crm/__manifest__.py
+++ solinda_crm/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','crm','sale','mail'],
+    'depends': ['base','crm','sale','mail','sol_cost_sheet'],
 
     # always loaded
     'data': [
