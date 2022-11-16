bc02efc8760e08cff4a9ceafd92a89b2a4ed9110
Luthfi Achmaddin Nizar <Luthfinizar97@gmail.com>
2022-11-16 15:44:59 +0700

    prob add by activity

--- solinda_crm/models/mail_activity.py
+++ solinda_crm/models/mail_activity.py
@@ -6,6 +6,15 @@ from odoo.tools.misc import clean_context
 class MailActivity(models.Model):
     _inherit = 'mail.activity'
     
+    def aaction_done(self):
+        res = super(MailActivity, self).aaction_done()
+        case = ['PO Received','Contract Signed','Contract Signed / PO Received','11. Contract Signed / PO Received','11.Contract Signed / PO Received']
+        if any(self.activity_type_id.name in n for n in case) and self.res_model == 'crm.lead':
+            crm_id = self.env["crm.lead"].search([("id", "=",self.res_id)])
+            crm_id.probability += self.activity_type_id.progress
+            crm_id.is_po_receive = True
+        return res
+
     def action_close_dialog(self):
         res = super(MailActivity, self).action_close_dialog()
         case = ['PO Received','Contract Signed','Contract Signed / PO Received','11. Contract Signed / PO Received','11.Contract Signed / PO Received']
